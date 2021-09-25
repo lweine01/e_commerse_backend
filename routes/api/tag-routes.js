254dce9af36c108d5f85a9cbcd4fa8858e6a4f7e
lweine01 <laurarweiner@gmail.com>
2021-09-25 17:20:43 -0400

    Completed all api/route GET POST PUT and DELETE operations.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -5,7 +5,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 router.get('/', async (req, res) => {
   try {
-    const tagData = await Tag.findAll({});
+    const tagData = await Tag.findAll({
+      include: {
+        model: Product,
+        attributes: [
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
@@ -14,7 +24,17 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
   try {
-    const tagData = await Tag.findByPk(req.params.id);
+    const tagData = await Tag.findByPk(req.params.id, {
+      include: {
+        model: Product,
+        attributes: [
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    });
     res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
